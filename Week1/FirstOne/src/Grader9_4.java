b89cf3e445f01e739562c508980684fe86e2808f
David Lash <dlash02@gmail.com>
2024-09-04 20:46:46 -0500

    update

---
+++ Week1/FirstOne/src/Grader9_4.java
@@ -0,0 +1,20 @@
+public class Grader9_4 {
+    public static void main(String[] args) {
+//        int x = 4;
+//        double y = 4.0;
+//        char grade = 'A';
+//        boolean flag = true;
+//        Integer x2 = 4;
+//        Double y2 = 4.0;
+//        Boolean bool = true;
+//        Character ch = 'a';
+//            String myName = "George";
+        int myX = 4;
+//        myX = myX + 1;
+//        myX++;
+//        myX += 2;
+//        ++myX;
+        System.out.printf("x=%s", myX++);
+        System.out.printf("x=%s", myX);
+    }
+}
---
+++ Week1/FirstOne/src/GraderV2_9_4.java
@@ -0,0 +1,48 @@
+import java.util.Scanner;
+
+public class GraderV2_9_4 {
+    public static void main(String[] args) {
+        // prompt user for 3 scores
+        // output their aver and the letter grade
+        Scanner sc = new Scanner(System.in);
+        int n = 1;
+        int gr1 = getGrade( sc, n++ );
+        int gr2 = getGrade( sc, n++ );
+        int gr3 = getGrade( sc, n++ );
+        double aver = getAverage( gr1, gr2, gr3 );
+        int lowest = getLowestScore( gr1, gr2, gr3 );
+        showAnswer( gr1, gr2, gr3, aver, lowest);
+    }
+
+    private static int getLowestScore(int gr1, int gr2, int gr3) {
+        return 0;
+    }
+
+    private static void showAnswer(int gr1, int gr2, int gr3, double aver, int lowest) {
+        System.out.printf("\nGr1:%s Gr2:%s Gr3:%s", gr1, gr2, gr3);
+        System.out.printf("\nAver:%2.2f ", aver);
+    }
+
+    private static double getAverage(int gr1, int gr2, int gr3) {
+        double aver = 0.0;
+        int sum = gr1 + gr2 + gr3;
+        aver = (double) sum / 3;
+        return aver;
+    }
+
+    private static int getGrade(Scanner s, int inN ) {
+        int grade = 0;
+        boolean keepGoing = true;
+        while (keepGoing) {
+            System.out.printf("\n Enter Grade%s:", inN);
+            try {
+                grade = s.nextInt();
+                keepGoing = false;
+            } catch (Exception e) {
+                System.out.printf("\n Invalid Grade");
+                s.nextLine();
+            }
+        }
+        return grade;
+    }
+}
